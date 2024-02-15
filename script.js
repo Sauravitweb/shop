dc88181124bff22c908c04f03c2c64cbff48bbc9
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2024-02-15 12:01:51 +0545

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+function showZoomedOptions(image) {
+    // Get the zoom container
+    var zoomContainer = document.getElementById('zoomContainer');
+
+    // Set the zoomed image source
+    var zoomedImage = zoomContainer.querySelector('.zoomed-image');
+    zoomedImage.src = image.src;
+
+    // Show the zoom container
+    zoomContainer.style.display = 'flex';
+
+    // Add event listener to close the zoomed image on click
+    zoomContainer.onclick = function() {
+      zoomContainer.style.display = 'none';
+    };
+}
